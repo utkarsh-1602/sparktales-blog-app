0af90daefc0acb1363c7ce631c912ab89a038f9f
utkarsh-1602 <usking619@gmail.com>
2024-01-29 03:45:32 +0530

    /api added

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post(`${baseURL}/auth/login`, {
+      const res = await axios.post(`${baseURL}/api/auth/login`, {
         username: userRef.current.value,
         password: passwordRef.current.value,
       });
