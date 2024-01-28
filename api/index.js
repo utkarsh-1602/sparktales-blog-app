36a58df8ddee1627124f690493bc41e0298a6b60
utkarsh-1602 <usking619@gmail.com>
2024-01-29 03:41:05 +0530

    cors updated

--- api/index.js
+++ api/index.js
@@ -14,11 +14,18 @@ const cors = require('cors');
 dotenv.config();
 
 // Allow requests from specific domain
-app.use(cors({
-  origin: 'https://sparktales-frontend.onrender.com/',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,  // Enable credentials (cookies, authorization headers, etc.)
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://sparktales-frontend.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 
 app.use(express.json());
