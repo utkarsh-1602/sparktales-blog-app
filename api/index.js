c23e4114decde0bffe2a99cf27b73cbf0f92e41d
utkarsh-1602 <usking619@gmail.com>
2024-01-29 00:42:33 +0530

    index.js fixed

--- api/index.js
+++ api/index.js
@@ -12,9 +12,10 @@ const cors = require('cors');
 
 
 dotenv.config();
+app.use(cors());
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors())
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -44,6 +45,8 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
-  console.log("Backend is running.");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend is running on port ${PORT}`);
 });
