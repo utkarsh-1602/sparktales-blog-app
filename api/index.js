f6825dcc3f040b5b9e20f1252238ad4d77ae50ab
utkarsh-1602 <usking619@gmail.com>
2024-01-29 02:26:12 +0530

    app.options deleted

--- api/index.js
+++ api/index.js
@@ -20,10 +20,6 @@ app.use(cors({
   credentials: true,  // Enable credentials (cookies, authorization headers, etc.)
 }));
 
-// Handle preflight requests
-app.options('*', cors());
-
-
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
